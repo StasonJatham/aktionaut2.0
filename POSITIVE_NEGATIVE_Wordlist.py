c6ea4d1cab04521917a83e4727f3670bffea860e
Karl <63649538+StasonJatham@users.noreply.github.com>
2020-04-14 21:11:22 +0200

    Add files via upload

---
+++ POSITIVE_NEGATIVE_Wordlist.py
@@ -0,0 +1,30 @@
+Continue to Surge Higher
+In Bullish Fashion
+Outpaces Stock Market Gains
+Soars Amid Report Of 
+Trades Higher After 
+Under Criminal Investigation
+Yanks A Bunch Of Cash From 
+Stock Inches Higher
+Chinese Hackers Target 
+Upgrades 
+Structural Improvements	
+Downgrades 
+Removes A Headwind
+Analyst Upgrade
+Analyst Downgrade
+Buy Rating
+Sell Rating				
+Surpasses																										
+Acquires 
+Expands Portfolio with Acquisition of
+expands portfolio with
+expands portfolio
+aquisition of 
+Completes Acquisition of Portfolio 
+to Acquire 
+Announce Completion of aquisition
+Review and Expected Closing of Transaction
+completes acquisition
+Completes Tender Offer to Acquire 
+to Acquire 100% of 
